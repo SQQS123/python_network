797fcae4c8fbc55573e96c7e24f20f120f562398
wuxiaojiang <717189544@qq.com>
2015-02-04 18:27:33 +0800

    Create test_google(v1.1).py

---
+++ test_google(v1.1).py
@@ -0,0 +1,18 @@
+import urllib2
+for i in range(99):
+    t = i+1
+    url = "http://210.242.125.%d"%t
+    req = urllib2.Request(url)
+    try:
+        google = urllib2.urlopen(req,data = None,timeout=1)
+        cheak = int(google.getcode())
+        if cheak == 200:
+            o = file('url.txt','a')
+            o.write(url)
+            o.write("\n")
+            o.flush()
+            o.close()
+            print '<%s> work!'%url
+    except(IOError):
+        #print "[%s] is not work"%url
+        pass

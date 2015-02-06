9508fb04c84bd9b6fce41c1d90981c0c36bbe84e
wuxiaojiang <717189544@qq.com>
2015-02-06 13:55:17 +0800

    Create scan_port.py

---
+++ scan_port.py
@@ -0,0 +1,15 @@
+import socket,sys
+ip = '192.168.1.103'
+ports = [21,22,53,80,443,445,3389,8080]
+for i in ports:
+    port = i
+    sock = socket.socket()
+    print "try to open<%s>:%s"%(ip,port)
+    try:
+        sock.connect((ip,port))
+        #sock.send('absdkfbsdafblabldsfdbfhasdflbf /n')
+        print "|___[+]"+ip+":"+str(port)+"   is open"
+        sock.close()
+    except(IOError):
+        #print "[-]"+ip+":"+str(port)+"not open/"
+        pass

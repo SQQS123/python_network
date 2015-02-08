ce4cf26ccb4ae80fce353d83a4382a58d4b9cbf2
wuxiaojiang <717189544@qq.com>
2015-02-08 21:02:50 +0800

    Revert "Update scan_port.py"
    This reverts commit d8858bff9fd910d92e1b7285ad504af246b5f511.

--- scan_port.py
+++ scan_port.py
@@ -1,30 +1,14 @@
-#-*-coding:cp936-*-
-import socket,sys,time
-t1 = time.time()
+import socket,sys
 ip = '192.168.1.103'
-#ports = [21,22,53,80,443,445,3389,8080]
-socket.setdefaulttimeout(0.3)
-c=[]
-d=0
-for i in range(1024):
-    port = i+1
+ports = [21,22,53,80,443,445,3389,8080]
+socket.setdefaulttimeout(2)
+for i in ports:
+    port = i
     sock = socket.socket()
-    print "尝试打开<%s>:%s"%(ip,port)
+    print "try to open<%s>:%s"%(ip,port)
     try:
         sock.connect((ip,port))
-        res = ip+":"+str(port)
-        print "|___[+]"+res+" 已打开"
-        c.append(res)
-        d=d+1
+        print "|___[+]"+ip+":"+str(port)+"   is open"
         sock.close()
     except(IOError):
         pass
-t2 = time.time()
-t = str(t2-t1)
-print "*****************************************"
-print "##################结果##################"
-print "共找到%s个打开的端口,耗时%ss"%(d,t)
-print "*****************************************"
-for i in c:
-    print i
-    print "*****************************************"

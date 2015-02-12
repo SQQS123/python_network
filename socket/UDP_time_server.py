69942a7b42cec7336e0c80aa3113fdd9ccd5b4c5
wuxiaojiang <717189544@qq.com>
2015-02-12 13:55:39 +0800

    holmes

--- socket/UDP_time_server.py
+++ socket/UDP_time_server.py
@@ -1,15 +1,20 @@
-import time,traceback,struct
+import time,sys
 import pysock
 host = ''
 port = 51423
 ser = pysock.new_server()
-s = ser.open_UDP()
-s.bind((host,port))
-print 'waiting for connection...'
 
-while 1:
-    message,address = s.recvfrom(8192)
-    secs = int(time.time())
-    secs += 2208988800
-    reply = struct.pack("!I",secs)
-    s.sendto(reply,address)
+while True:
+    print 'waiting for connection...'
+    try:
+        s = ser.open_UDP()
+        s.bind((host,port))
+        message,address = s.recvfrom(8192)
+        print "got data from %s"%str(address)
+        print message
+        secs = int(time.time())
+        secs += 2208988800
+        s.sendto(str(secs),address)
+        s.close()
+    except(IOError):
+        sys.exit(1)
--- socket/client(UDP).py
+++ socket/client(UDP).py
@@ -3,5 +3,10 @@ ip = '192.168.1.103'
 port = 51423
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client.connect((ip,port))
-print client.recv(1024)
+client.sendall('hello')
+try:
+    print client.recv(1024)
+except(IOError):
+    print 'lose'
+    pass
 client.close()

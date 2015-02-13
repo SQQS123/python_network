cefbc076871d2048ff484134dda420db28df0702
wuxiaojiang <717189544@qq.com>
2015-02-13 14:01:32 +0800

    holmes

--- socket/UDP_shell.py
+++ socket/UDP_shell.py
@@ -1,4 +1,5 @@
 import pysock,subprocess
+
 host = ''
 port = 51423
 ser = pysock.new_server()
@@ -10,6 +11,16 @@ while True:
         print 'waiting for connection...'
         message,address = s.recvfrom(1024)
         print 'get code <%s> from %s'%(message,str(address))
-        p = subprocess.Popen(message,shell=True)
+        p = subprocess.Popen(message,
+                             shell=True,
+                             stdout = subprocess.PIPE,
+                             stdin = subprocess.PIPE,
+                             stderr = subprocess.STDOUT)
+        STDOUT,STDERR = p.communicate()
+        if STDOUT:
+            s.sendto(STDOUT,address)
+        else:
+            print '0'
+        
     except(IOError):
         pass
--- socket/client(UDP).py
+++ socket/client(UDP).py
@@ -4,11 +4,12 @@ port = 51423
 ser = pysock.new_server()
 client = ser.open_UDP()
 client.connect((ip,port))
-code = raw_input('code')
-client.sendall(code)
-try:
-    print client.recv(1024)
-except(IOError):
-    print 'lose'
-    pass
-client.close()
+while True:
+    code = raw_input('code')
+    client.sendall(code)
+    try:
+        print client.recv(1024)
+    except(IOError):
+        print 'lose'
+        pass
+

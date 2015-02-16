ec007de36fcd7dab0f3490ec6f047abba2fa2285
wuxiaojiang <717189544@qq.com>
2015-02-16 17:20:14 +0800

    holmes

--- socket/dns.py
+++ socket/dns.py
@@ -1,16 +1,18 @@
 import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+list1 = []
 #q = b'>:\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07google\x03com\x00\x00\x01\x00\x01'
-q = b'\x5c\x6d\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x05baidu\x03com\x00\x00\x01\x00\x01'
+q = b'\x5c\x6d\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x08facebook\x03com\x00\x00\x01\x00\x01'
 print q
 print '----------------------------'
 #s.sendto(q,('208.67.222.222',53))
 s.sendto(q,('8.8.8.8',53))
 c = s.recv(4096)
-print c
 for i in c:
     t = ord(i)
-    #print '%x'%t
     print t
+    list1.append(str(t))
 s.close
+#str1 = '.'.join(list1)
+print str1
 

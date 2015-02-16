da1c5cfcc0db377b89a6312016f2868435e5e6ac
wuxiaojiang <717189544@qq.com>
2015-02-16 17:01:37 +0800

    holmes

--- socket/dns.py
+++ socket/dns.py
@@ -1,7 +1,16 @@
 import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-q = b'>:\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07baidu\x03com\x00\x00\x01\x00\x01'
-s.sendto(q,('10.89.64.5',53))
-print s.recv(4096)
+#q = b'>:\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07google\x03com\x00\x00\x01\x00\x01'
+q = b'\x5c\x6d\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x05baidu\x03com\x00\x00\x01\x00\x01'
+print q
+print '----------------------------'
+#s.sendto(q,('208.67.222.222',53))
+s.sendto(q,('8.8.8.8',53))
+c = s.recv(4096)
+print c
+for i in c:
+    t = ord(i)
+    #print '%x'%t
+    print t
 s.close
-raw_input()
+
